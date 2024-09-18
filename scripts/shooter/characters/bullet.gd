5bd1ba259af0a02bf5e5214ebe08aa88e1f748f2
AlonsoElPro10 <alonso.becerra.uchile@gmail.com>
2024-09-18 12:40:09 -0300

    now enemies can die

--- scripts/shooter/characters/bullet.gd
+++ scripts/shooter/characters/bullet.gd
@@ -1,6 +1,15 @@
 extends Area2D
 
 var speed = -500  # Velocidad que aplicaremos después
+var damage = 10 #Daño que hace la bala
 		
 func _physics_process(delta: float) -> void:
 	position += transform.y * speed * delta
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_bullet_body_entered"))
+
+func _on_bullet_body_entered(body):
+	if body.is_in_group("enemies"):  
+		body.take_damage(damage)  
+		queue_free()  # Destruye la bala después de colisionar
--- scripts/shooter/characters/enemy.gd
+++ scripts/shooter/characters/enemy.gd
@@ -1,5 +1,17 @@
 class_name Enemy
 extends CharacterBody2D
+var health = 20
 
 func _ready() -> void:
+	add_to_group("enemies") #asegurarse que se añadan al grupo de enemigos
 	scale.y = -1
+	
+
+
+func take_damage(amount):
+	health = health - amount #daño que le quita
+	if health <= 0:
+		die()
+
+func die():
+	queue_free()  # Desaparecer la escena de la bala cuando el enemigo no tenga vida
