9c6ac901520f1e9c1def073bede183f6dbb5deab
diego-acevedo <dacevedom@live.com.mx>
2024-10-05 00:32:36 -0300

    feat: add elements to UI

--- scripts/rythm/rythm.gd
+++ scripts/rythm/rythm.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var bonus_meter: TextureProgressBar = $UI/Meters/BonusMeter
+@onready var fuel_meter: Sprite2D = $UI/Meters/FuelMeter
+@onready var bonus_icon: Sprite2D = $UI/Meters/BonusIcon
+
 func _input(event: InputEvent) -> void:
 	
 	# Un input para cerrar el juego con tan solo apretar la tecla "ESC".
@@ -11,3 +15,13 @@ func _input(event: InputEvent) -> void:
 func pass_to_main():
 	get_parent().correct_to_shoot()
 	
+
+func _physics_process(_delta: float) -> void:
+	if (bonus_meter.value < 100):
+		bonus_meter.value += 1
+	else:
+		bonus_meter.value = 0
+		
+	fuel_meter.rotation = 1.5*sin(Time.get_unix_time_from_system())
+	
+	bonus_icon.frame = int (Time.get_unix_time_from_system()) % 5
