dca3e07d1cfce8b31a50910f45a1cbec3e3f5025
diego-acevedo <dacevedom@live.com.mx>
2024-10-12 15:40:12 -0300

    fix: telepoting enemies when they died

--- scripts/shooter/characters/patterns/circle.gd
+++ scripts/shooter/characters/patterns/circle.gd
@@ -2,20 +2,19 @@ extends Pattern
 
 @export var width: int = 50
 @export var height: int = 50
-var angles: Array[float] = []
+var angles = {}
 
 func _ready() -> void:
 	super._ready()
 	for i in range(enemies.size()):
 		var enemy = enemies[i]
-		angles.append(2 * PI / enemiesScenes.size() * i)
-		enemy.position.x = width * sin(angles[i])
-		enemy.position.y = height * cos(angles[i])
+		angles[enemy] = 2 * PI / enemiesScenes.size() * i
+		enemy.position.x = width * sin(angles[enemy])
+		enemy.position.y = height * cos(angles[enemy])
 	
 func _physics_process(delta: float) -> void:
-	for i in range(enemies.size()):
-		var enemy = enemies[i]
-		var radius = sqrt(pow(width * sin(angles[i]), 2) + pow(height * cos(angles[i]), 2))
-		angles[i] += speed / radius * delta
-		enemy.position.x = width * sin(angles[i])
-		enemy.position.y = height * cos(angles[i])
+	for enemy in enemies:
+		var radius = sqrt(pow(width * sin(angles[enemy]), 2) + pow(height * cos(angles[enemy]), 2))
+		angles[enemy] += speed / radius * delta
+		enemy.position.x = width * sin(angles[enemy])
+		enemy.position.y = height * cos(angles[enemy])
--- scripts/shooter/characters/patterns/linear.gd
+++ scripts/shooter/characters/patterns/linear.gd
@@ -2,18 +2,17 @@ extends Pattern
 
 @export var width: int = 100
 @export var height: int = 200
-@export var angles: Array[float]
+var angles = {}
 
 func _ready() -> void:
 	super._ready()
 	for i in range(enemies.size()):
 		var enemy = enemies[i]
-		angles.append(PI / enemiesScenes.size() * i)
-		enemy.position.x = width * i / enemies.size() - (width / 2)
+		angles[enemy] = PI / enemiesScenes.size() * i
+		enemy.position.x = width * sin(angles[enemy])
 		enemy.position.y = height * i / enemies.size() - (height / 2)
 
 func _physics_process(delta: float) -> void:
-	for i in range(enemies.size()):
-		var enemy = enemies[i]
-		angles[i] += speed / width * delta
-		enemy.position.x = width * sin(angles[i])
+	for enemy in enemies:
+		angles[enemy] += speed / width * delta
+		enemy.position.x = width * sin(angles[enemy])
