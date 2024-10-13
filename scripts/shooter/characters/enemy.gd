90b5a2cbd4dffbf5daf7448922bf2d696d6feb00
AlonsoElPro10 <alonso.becerra.uchile@gmail.com>
2024-10-12 22:49:46 -0300

    add die animation

--- scripts/shooter/characters/enemy.gd
+++ scripts/shooter/characters/enemy.gd
@@ -1,6 +1,7 @@
 class_name Enemy
 extends CharacterBody2D
 @export var health = 10
+@onready var animation_player: AnimationPlayer = $Spritesheet/AnimationPlayer
 
 signal dead(unit)
 
@@ -12,7 +13,13 @@ func take_damage(amount):
 	health = health - amount #daño que le quita
 	if health <= 0:
 		die()
-
+		
 func die():
 	dead.emit(self)
-	queue_free()  # Desaparecer la escena de la bala cuando el enemigo no tenga vida
+	animation_player.play("explosion")
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 0.25  # Tiempo de espera (0.2 segundos)
+	timer.one_shot = true
+	timer.start()
+	timer.timeout.connect(func(): queue_free())
