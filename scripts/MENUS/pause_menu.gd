5f6c4ba2aa1cfa559c5d505e8661ade44927401a
diego-acevedo <dacevedom@live.com.mx>
2024-10-25 11:49:54 -0300

    feat: add buttons to pause menu

--- scripts/MENUS/pause_menu.gd
+++ scripts/MENUS/pause_menu.gd
@@ -1,11 +1,16 @@
 extends CanvasLayer
 
 @onready var resume: Button = $VBoxContainer/Resume
+@onready var restart: Button = $VBoxContainer/Restart
+@onready var main_menu: Button = $"VBoxContainer/Main Menu"
+@onready var quit: Button = $VBoxContainer/Quit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	resume.pressed.connect(_on_resume_pressed)
-
+	restart.pressed.connect(_on_restart_pressed)
+	main_menu.pressed.connect(_on_main_menu_pressed)
+	quit.pressed.connect(_on_quit_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event: InputEvent) -> void:
@@ -16,3 +21,12 @@ func _input(event: InputEvent) -> void:
 func _on_resume_pressed() -> void:
 	hide()
 	get_tree().paused = false
+
+func _on_restart_pressed() -> void:
+	pass
+	
+func _on_main_menu_pressed() -> void:
+	pass
+	
+func _on_quit_pressed() -> void:
+	get_tree().quit()
