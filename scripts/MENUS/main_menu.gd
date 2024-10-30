99ab4fc9f9a7aab69548385c6604735ce4798ae5
Jorge <jorge.contreras.g@ug.uchile.cl>
2024-10-29 21:20:29 -0300

    :sparkles: feat: Add functional Main Menu

---
+++ scripts/MENUS/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var go_to_demo_button: Button = $"GO TO DEMO BUTTON"
+@onready var credits_button: Button = $"CREDITS BUTTON"
+@onready var quit_button: Button = $"QUIT BUTTON"
+
+var demo_scene: PackedScene = preload("res://scenes/main.tscn")
+
+func _ready() -> void:
+	go_to_demo_button.pressed.connect(_on_go_to_demo_pressed)
+	credits_button.pressed.connect(_on_credits_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+	
+func _on_go_to_demo_pressed():
+	get_tree().change_scene_to_packed(demo_scene)
+	
+func _on_quit_pressed():
+	get_tree().quit()
+	
+func _on_credits_pressed():
+	get_tree().change_scene_to_packed(demo_scene)
